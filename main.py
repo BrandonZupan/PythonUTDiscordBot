8fb2ec4d14147ed22eb723db7b2e41e0853285f2
Brandon Zupan <zupan.brandon@utexas.edu>
2019-06-04 22:37:09 -0500

    Added ability to get multiple arguments

--- main.py
+++ main.py
@@ -10,16 +10,23 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
+    
+    #Check if they're talking to bot
+    if message.content.startswith('$'):
+        #Break up the message text into an array
+        command = message.content.split()
+        print(command)
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+        #Hello: Replies hello
+        if message.content.startswith('$hello'):
+            await message.channel.send('Hello!')
 
-    if message.content.startswith('$updateicon'):
-        with open("white.png", "rb") as image:
-            f = image.read()
-            b = bytearray(f)
-            await message.guild.edit(icon=b)
-            await message.channel.send("Icon set to white")
+        if message.content.startswith('$updateicon'):
+            with open("white.png", "rb") as image:
+                f = image.read()
+                b = bytearray(f)
+                await message.guild.edit(icon=b)
+                await message.channel.send("Icon set to white")
 
 keyFile = open('keys.txt', 'r')
 key = keyFile.read()
