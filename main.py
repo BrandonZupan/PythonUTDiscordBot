15b3f575db2cbb1fc8ee909a6439773c2d6f0600
Brandon Zupan <zupan.brandon@utexas.edu>
2019-06-08 21:07:48 +0000

    Added comment

--- main.py
+++ main.py
@@ -9,47 +9,12 @@ client = commands.Bot(command_prefix='$')
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
-"""
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    
-    #Check if they're talking to bot
-    if message.content.startswith('$'):
-        #Break up the message text into an array of lowercase letters
-        command = message.content.lower()
-        command = command.split()
-        print(command)
-
-        if command[0] == "$updateicon":
-            #Check if admin
-            if message.author.guild_permissions.administrator:
-
-                #See if we are auto updating
-                if command[1] == "auto":
-                    command[1] = await on_updatecolor(message)
-
-                try:
-                    with open(command[1] + ".png", "rb") as image:
-                        f = image.read()
-                        b = bytearray(f)
-                        await message.guild.edit(icon=b)
-                        await message.channel.send("Icon set to " + command[1])
-                #If the file isn't found, then the tower color is probably unknown
-                except FileNotFoundError:
-                    await message.channel.send("Error: Unknown tower color. " + 
-                    "Options are white, orange, orangewhite, and dark")
-                #No option was entered
-                except IndexError:
-                    await message.channel.send("Please enter an argument")
-
-            else: 
-                await message.channel.send("You do not have permission to do that")
-"""
-
 async def is_admin(ctx):
     return ctx.message.author.guild_permissions.administrator
+    
+##############
+###Commands###
+##############
 
 @client.command(name='hello')
 async def hello(ctx):
