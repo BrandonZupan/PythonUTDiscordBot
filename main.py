4bfa9931de51666fbfa0440d942cd804526150cc
Brandon Zupan <zupan.brandon@utexas.edu>
2019-06-20 13:55:32 -0500

    Turned off echo

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 #SQL Database
-engine = create_engine('sqlite:///responces.db', echo=True)
+engine = create_engine('sqlite:///responces.db', echo=False)
 Base = declarative_base()
 
 class ccCommand(Base):
