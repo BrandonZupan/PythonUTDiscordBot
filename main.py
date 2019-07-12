88b67ecf0e58eb4ed90620e88e69a85407c6a272
Brandon Zupan <zupan.brandon@utexas.edu>
2019-07-12 09:58:53 -0500

    Removed '' in welcome message

--- main.py
+++ main.py
@@ -399,7 +399,7 @@ async def on_member_join(ctx):
     newUserMessage = f"Welcome to the UT Austin Discord {ctx.mention}!  Please select which school/college you are in by using `$rank [school/college]` in the #bot-commands channel. "
     newUserMessage += "You can also select your graduating class and where you will be living.  "
     newUserMessage += "See the list of available schools/colleges, graduating classes, and communities by using `$ranks.` \n"
-    newUserMessage += "If you are having problems joining a rank, please message a member with the 'Founder or Moderator' role.  "
+    newUserMessage += "If you are having problems joining a rank, please message a member with the Founder or Moderator role.  "
     newUserMessage += "Be sure to read our rules in #real-rules."
     await ctx.send(newUserMessage)
     logging.info(f"Sent join PM to {ctx.mention}")
